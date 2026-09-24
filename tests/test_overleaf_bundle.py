b023c550ca8070130ed661a758907445a0b90f19
Rocky 文浩 <70815297+Rocky5502@users.noreply.github.com>
2026-09-24 17:03:05 +0800

    Test new hosted black-box figure bundle path

--- tests/test_overleaf_bundle.py
+++ tests/test_overleaf_bundle.py
@@ -48,4 +48,4 @@ def test_generated_fairsynth_figure_is_an_optional_artifact_input():
 
 def test_hosted_pilot_operational_artifacts_are_optional_bundle_inputs():
     assert "generated/hosted_pilot_operational_table.tex" in OPTIONAL_RESULT_FILES
-    assert "figures/hosted_pilot_cost_profile.pdf" in OPTIONAL_RESULT_FILES
+    assert "figures/hosted_pilot_blackbox_summary.pdf" in OPTIONAL_RESULT_FILES
